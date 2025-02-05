4a5d452b37ca4fdd77fd3e23b78190deffe815ef
juanj72 <juan.jara@campusucc.edu.co>
2025-02-05 17:31:25 -0500

    chore: refactor main to fix linter errors

--- app/main.py
+++ app/main.py
@@ -9,9 +9,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-
 @app.get("/concat/{c1}/{c2}")
 async def concat_chars(c1: str, c2: str):
     result = c1 + c2
     return JSONResponse(content={"concatenated": result})
-
